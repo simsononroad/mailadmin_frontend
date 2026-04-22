c5d33d569c87a023ed8401bd20e0c704f45b879b
simsononroad <d-github@kopaszhegy.hu>
2026-04-22 21:41:29 +0200

    Add fix bootstrap files bootstrap version: 5.3.8
    Add aliasok template for displaying and adding aliases
    
    - Created a new HTML template 'aliasok.html' that extends the base template.
    - Implemented a list to display existing aliases with a scrollable view.
    - Added a form to allow users to submit new aliases with a predefined domain.

--- app.py
+++ app.py
@@ -10,12 +10,39 @@ tovabbitok = ["valami", "test", "aliasez","ezis","valami", "test", "aliasez","ez
 emailek = ["valami@test.com", "valami@test1.com", "valami@test2.com", "valami@test3.com", "valami@test4.com", "valami@test5.com", "valami@test3.com", "valami@test4.com", "valami@test5.com"]
 domainek = ["google.com", "github.com", "youtube.com", "example.com", "google.com", "github.com", "youtube.com", "example.com"]
 
+"""
 
+Input name tags:
+    - aliasok.html > (add alias inp): alias_name
+
+"""
 
 
 @app.route("/")
 def index():
     return render_template("index.html", aliasok=aliasok, tovabbitok=tovabbitok, emailek=emailek, domainek=domainek)
+
+
+@app.route("/alias")
+def alias():
+    return render_template("aliasok.html", aliasok=aliasok)
+
+
+
+
+
+# Funkcionalitás
+
+@app.route("/add_alias", methods=["POST"])
+def add_alias():
+    alias_name = request.form.get('alias_name')
+    if alias_name == "":
+        flash("Hiba! Üres a bemeneti mező", "danger")
+        return redirect(url_for("alias"))
+    aliasok.append(alias_name)
+    return redirect(url_for("alias"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
